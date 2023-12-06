973580fd7edd3ea6b5b83a3130ec39624de70631
gd3kr <aaryagadekar01@gmail.com>
2023-12-06 01:43:55 -0600

    built server but outputs are unique even with a generator. find out why

--- Mac/actualServer.py
+++ Mac/actualServer.py
@@ -12,6 +12,16 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import torch
 from PIL import Image
+# import flask 
+from flask import Flask, request, jsonify
+from PIL import Image
+from io import BytesIO
+import io
+from flask import Response
+
+app = Flask(__name__)
+
+
 
 
 adapter_id = "latent-consistency/lcm-lora-sdv1-5"
@@ -19,11 +29,12 @@ base_model = "lambdalabs/sd-image-variations-diffusers"
 
 
 device = torch.device("mps")
-generator = torch.Generator(device).manual_seed(42)
+generator = torch.Generator(device).manual_seed(0)
 image_encoder = CLIPVisionModelWithProjection.from_pretrained(base_model, subfolder="image_encoder").to(device=device)
 feature_extractor = CLIPImageProcessor.from_pretrained(base_model, subfolder="feature_extractor")
 do_classifier_free_guidance = True
 dtype = next(image_encoder.parameters()).dtype
+
 
 
 def encode_text(text):
@@ -73,6 +84,7 @@ def load_model():
     )
     pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
     pipe.text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14").to(device=device)
+    pipe.generator = torch.Generator(device).manual_seed(0)
 
     pipe.unet.set_attn_processor(AttnProcessor2_0())
 
@@ -84,8 +96,6 @@ def load_model():
     pipe.fuse_lora()
 
     return pipe
-
-
 
 def get_latents(image):
 
@@ -101,8 +111,8 @@ def get_latents(image):
         [0.26862954, 0.26130258, 0.27577711]),
     ])
 
-    text="flowers"
-    output= encode_image(image_encoder, feature_extractor, dtype, tform(image).unsqueeze(0), text, device, 1)
+    # text="flowers"
+    output= encode_image(image_encoder, feature_extractor, dtype, tform(image).unsqueeze(0), "", device, 1)
     return output
 
 
@@ -134,124 +144,91 @@ def generate_image(latents): # takes in latents as input and generates an image 
   return images
 
 
-def latent_processing():
-    import glob
-    import os
-    oceans = []
-    barren = []
-    ocean_dir = './workspace/oceans'
-    barren_dir = './workspace/barren'
-    ocean_paths_jpg = glob.glob(os.path.join(ocean_dir, '*.jpg'))
-    ocean_paths_png = glob.glob(os.path.join(ocean_dir, '*.png'))
-    barren_paths_jpg = glob.glob(os.path.join(barren_dir, '*.jpg'))
-    barren_paths_png = glob.glob(os.path.join(barren_dir, '*.png'))
 
-    # Combine the lists of paths
-    ocean_paths = ocean_paths_jpg + ocean_paths_png
-    barren_paths = barren_paths_jpg + barren_paths_png
-
-    ocean_latents = []
-    for image_path in ocean_paths:
-        image = Image.open(image_path).convert('RGB')
-        ocean_latents.append(get_latents(image))
-
-    barren_latents = []
-    for image_path in barren_paths:
-        image = Image.open(image_path).convert('RGB')
-        barren_latents.append(get_latents(image))
-
-
-    mean_ocean_embeddings = torch.mean(torch.stack(ocean_latents), dim=0)
-    print(mean_ocean_embeddings.shape)
-
-
-    mean_barren_embeddings = torch.mean(torch.stack(barren_latents), dim=0)
-    print(mean_barren_embeddings.shape)
-    return mean_ocean_embeddings, mean_barren_embeddings
-
-
-
-
-# mean_ocean_embeddings, mean_barren_embeddings = latent_processing()
-
-# store the mean embeddings in a file
-# torch.save(mean_ocean_embeddings, "mean_ocean_embeddings.pt")
-# torch.save(mean_barren_embeddings, "mean_barren_embeddings.pt")
-
-# exit(1)
-
-# load the mean embeddings from a file
-mean_ocean_embeddings = torch.load("mean_ocean_embeddings.pt")
-mean_barren_embeddings = torch.load("mean_barren_embeddings.pt")
-
+app = Flask(__name__)
 pipe = load_model().to(device)
-#Eiffel Tower
-# url = "https://media.cntraveler.com/photos/58de89946c3567139f9b6cca/1:1/w_3633,h_3633,c_limit/GettyImages-468366251.jpg"
 
-#new york
-# url = "https://www.travelandleisure.com/thmb/91pb8LbDAUwUN_11wATYjx5oF8Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/new-york-city-evening-NYCTG0221-52492d6ccab44f328a1c89f41ac02aea.jpg"
+def mix_images(image_a, image_b, mix_value):
+    return image_a * (1 - mix_value) + image_b * mix_value
 
-#collosseum
-# url = "https://miro.medium.com/v2/resize:fit:1198/1*W_quBAVAMu8a4n1Sg07Xvw.jpeg"
+latents_store = {}
 
-# Taj Mahal URL
-# url = "https://th-thumbnailer.cdn-si-edu.com/NaExfGA1op64-UvPUjYE5ZqCefk=/fit-in/1600x0/filters:focal(1471x1061:1472x1062)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/b6/30/b630b48b-7344-4661-9264-186b70531bdc/istock-478831658.jpg"
-
-#fish
-# url = "https://media.istockphoto.com/id/699306668/photo/underwater-scene-with-coral-reef-and-tropical-fish.jpg?s=612x612&w=0&k=20&c=HstAcsVjlUp1vNLni0_YpYRRODiBrXMC2FGUKKKZZuk="
-
-#pinkney
-url = "https://www.justinpinkney.com/img/YOBg9JfLdN-512.webp"
-
-image = Image.open(requests.get(url, stream=True).raw)
-
-image = Image.open("ghibli.jpg").convert('RGB')
-
-start_embedding = get_latents(image)
-positive_to_negative = + mean_ocean_embeddings - mean_barren_embeddings
-text_embedding_1 = encode_text("oil painting").unsqueeze(0)
-text_embedding_2 = encode_text("dslr leica photo").unsqueeze(0)
-
-
-
-images = [] # List to store generated images
-for t in torch.linspace(0,0.5, 10):   
+@app.route('/store_latent', methods=['POST'])
+def store_latent():
+    data = request.get_json()
+    image_url = data['image_url']
+    image_id = data['id']
     
-    # embedding = start_embedding + (text_embedding_1 - text_embedding_2) * t
+    if (not image_url or not image_id):
+        return jsonify({'error': "bruh"}), 500
 
-    # weighted mean
-    embedding = start_embedding*(1-t) + text_embedding_2 * t
+    
+    try:
+        response = requests.get(image_url)
+        response.raise_for_status()
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    except requests.RequestException as e:
+        return jsonify({'error': str(e)}), 500
 
-    # embedding = slerp(start_embedding, start_embedding + text_embeddings, t)
-    print(f'negative × {t:.2f}')
-    img = generate_image(embedding).images[0]
-    # save image
-    img.save(f"2_{t:.2f}.jpg")
-    images.append(img)
+    latent = get_latents(image)
+    latents_store[image_id] = latent
+
+    return jsonify({'message': 'latent stored', 'id': image_id}), 200
 
 
-# for i in range(10):
-#    # merge embedding and direction without slerp
-#     embedding = start_embedding + positive_to_negative * i / 10
-#     print(f'negative × {i:.2f}')
-#     img = generate_image(embedding).images[0]
-#     # save image
-#     img.save(f"output_{i:.2f}.jpg")
-#     images.append(img)
+@app.route('/slerp', methods=['POST'])
+def slerp_route():
 
-# Concatenate images horizontally
-concatenated_image = Image.new('RGB', (images[0].width * len(images), images[0].height))
-x_offset = 0
-for img in images:
-    concatenated_image.paste(img, (x_offset, 0))
-    x_offset += img.width
+    data = request.get_json()
+    id_a = data['id_a']
+    id_b = data['id_b']
+    n = float(data['mix_value'])
 
-# Display the concatenated image
-plt.figure(figsize=(50, 5))  # Adjust the figure size as needed
-# save image
-concatenated_image.save("output.jpg")
+    if id_a not in latents_store or id_b not in latents_store:
+        return jsonify({'error': 'one or both ids do not exist in the latent store'}), 400
 
-plt.imshow(concatenated_image)
-plt.axis('off')  # Hide axes
-plt.show()
+    if not (0 <= n <= 1):
+        return jsonify({'error': 'n must be between 0 and 1'}), 400
 
+    start_embedding = latents_store[id_a]
+    end_embedding = latents_store[id_b]
+    interpolated_latent = slerp(start_embedding, end_embedding, n)
+    interpolated_image = generate_image(interpolated_latent).images[0]
+
+    img_byte_arr = io.BytesIO()
+    interpolated_image.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+
+    return Response(img_byte_arr, mimetype='image/jpeg')
+
+
+@app.route('/mix', methods=['POST'])
+def mix():
+    data = request.get_json()
+    id_a = data['id_a']
+    id_b = data['id_b']
+    mix_value = float(data['mix_value'])
+    
+
+    if not (0 <= mix_value <= 1):
+        return jsonify({'error': 'mix_value must be between 0 and 1'}), 400
+
+    if id_a not in latents_store or id_b not in latents_store:
+        return jsonify({'error': 'one or both ids do not exist in the latent store'}), 400
+
+    image_a_latent = latents_store[id_a]
+    image_b_latent = latents_store[id_b]
+
+    mixed_latent = mix_images(image_a_latent, image_b_latent, mix_value)
+    mixed_image = generate_image(mixed_latent).images[0]
+
+    img_byte_arr = io.BytesIO()
+    mixed_image.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+    # send as jpg image
+
+
+    return Response(img_byte_arr, mimetype='image/jpeg')
+
+if __name__ == '__main__':
+    app.run(debug=True)

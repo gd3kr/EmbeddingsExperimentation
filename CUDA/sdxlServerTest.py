c52ed64a3f8e17f03943a3537f136b2bdd99f5b2
gd3kr <aaryagadekar01@gmail.com>
2023-12-06 23:55:43 -0600

    implemented custom pipeline with IP adapter. Need to bypass image encoding and feed in image latents directly now

--- CUDA/sdxlServerTest.py
+++ CUDA/sdxlServerTest.py
@@ -5,12 +5,11 @@ from diffusers.utils import load_image
 model_id =  "sd-dreambooth-library/herge-style"
 
 # pipe = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
-pipe.to("cuda")
+pipe = AutoPipelineForText2Image.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
+pipe.to("mps")
 
 
-pipe.load_ip_adapter("h94/IP-Adapter", subfolder="sdxl_models", weight_name="ip-adapter_sdxl.bin")
-pipe.enable_model_cpu_offload()
+pipe.load_ip_adapter("h94/IP-Adapter", subfolder="models", weight_name="ip-adapter_sd15.bin")
 
 prompt = "best quality, high quality"
 image = load_image("https://user-images.githubusercontent.com/24734142/266492875-2d50d223-8475-44f0-a7c6-08b51cb53572.png")
@@ -21,7 +20,7 @@ image = load_image("https://user-images.githubusercontent.com/24734142/266492875
 #     guidance_scale=1,
 # ).images[0]
 
-images = pipe(ip_adapter_image=image, prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+images = pipe(ip_adapter_image=image, prompt=prompt, num_inference_steps=4, guidance_scale=0.0).images[0]
 
 
 
